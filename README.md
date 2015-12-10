66b27c36fbb9c80980c68780ca9737131c3be851
Dennis Schwartz <schwartz_dennis@web.de>
2015-12-10 15:14:01 +0000

    Initial commit. Functional model creation - very slow\!

---
+++ README.md
@@ -0,0 +1,65 @@
+# biojs-io-munemo
+
+[![NPM version](http://img.shields.io/npm/v/biojs-io-munemo.svg)](https://www.npmjs.org/package/biojs-io-munemo) 
+[![Build Status](https://secure.travis-ci.org/DennisSchwartz/biojs-io-munemo.png?branch=master)](http://travis-ci.org/DennisSchwartz/biojs-io-munemo) 
+
+> A model for multilayer networks
+
+## Getting Started
+Install the module with: `npm install biojs-io-munemo`
+
+```javascript
+var munemo = require('biojs-io-munemo');
+munemo.hello("biojs"); // "hello biojs"
+```
+
+## Documentation
+
+#### .hello(name)
+
+**Parameter**: `name`
+**Type**: `String`
+**Example**: `biojs`
+
+The 'hello' method is responsible for showing a name.
+
+How to use this method
+
+```javascript
+munemo.hello('biojs'); // "hello biojs"
+```
+
+## Contributing
+
+All contributions are welcome.
+
+## Support
+
+If you have any problem or suggestion please open an issue [here](https://github.com/DennisSchwartz/biojs-io-munemo/issues).
+
+## License 
+
+The MIT License
+
+Copyright (c) 2015, Dennis Schwartz
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/test_munemo.js
@@ -0,0 +1,78 @@
+/*
+ * biojs-io-munemo
+ * https://github.com/DennisSchwartz/biojs-io-munemo
+ *
+ * Copyright (c) 2015 Dennis Schwartz
+ * Licensed under the MIT license.
+ */
+
+// chai is an assertion library
+var chai = require('chai');
+var R = require('ramda');
+var Mplexnet = require('mplexnet').Network;
+var fs = require('fs');
+var time = require('node-tictoc');
+
+// @see http://chaijs.com/api/assert/
+var assert = chai.assert;
+
+// register alternative styles
+// @see http://chaijs.com/api/bdd/
+chai.expect();
+chai.should();
+
+// requires your main app (specified in index.js)
+var munemo = require('../');
+
+describe('biojs-io-munemo module', function(){
+    describe('Functional network generation', function () {
+        var elements;
+        before(function () {
+            elements = munemo({
+                inFormat: 'csv',
+                data: 'source,sourceLayer,target,targetLayer \n A,1,A,2 \n B,1,B,2 \n C,1,C,2 \n D,1,D,2 \n \
+                A,1,C,1 \n B,1,C,1 \n B,1,D,1 \n C,1,D,1 \n A,2,B,2 \n B,2,D,2 \n C,2,A,2 \n C,2,D,2'
+            });
+        });
+        it ('should contain 12 edges', function () {
+            var edges = R.filter(function (e) { return e.group === 'edge'; }, elements);
+            chai.expect(edges.length).to.equal(12);
+        });
+        it ('should contain 4 nodes', function () {
+            var nodes = R.filter(function (e) { return e.group === 'node'; }, elements);
+            chai.expect(nodes.length).to.equal(4);
+        });
+        it ('should contain 8 node-layers', function () {
+            var nodelayers = R.filter(function (e) { return e.group === 'nodelayer'; }, elements);
+            chai.expect(nodelayers.length).to.equal(8);
+        });
+        it ('should contain 2 layers', function () {
+            var layers = R.filter(function (e) { return e.group === 'layer'; }, elements);
+            chai.expect(layers.length).to.equal(2);
+        })
+    })
+});
+
+describe('speed comparison mplexnet - munemo', function () {
+    var file;
+    var fileMunemo;
+    before(function () {
+        // get the File
+        //fileMunemo = fs.readFileSync('test/5000-munemo.csv');
+        time.tic();
+        file = fs.readFileSync('test/arabidopsis1000.csv', 'utf-8');
+        console.log("Read File");
+        time.toc();
+    });
+    it ('should compare time', function () {
+        // Create the network with mplexnet
+        time.tic();
+        var mplexnet = new Mplexnet({ options: { inputFormat: 'csv'} , data: file });
+        console.log('Mplexnet: ');
+        time.toc();
+        time.tic();
+        var elements = munemo( { inFormat: 'csv', data: file } );
+        console.log('Munemo: ');
+        time.toc();
+    });
+});
